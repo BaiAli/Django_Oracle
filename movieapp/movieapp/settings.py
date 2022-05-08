49774167865e32c45ce2641d538ba2c51b02dbf4
BaiAli <baizhumanali@gmail.com>
2022-05-08 13:20:28 +0600

    'adding'

--- movieapp/movieapp/settings.py
+++ movieapp/movieapp/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR / 'templates'
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'movieapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,7 +83,7 @@ DATABASES = {
         'PASSWORD': 'hr',
     }
 }
-
+# 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- movieapp/moviesite/views.py
+++ movieapp/moviesite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def home(request):
+    data = Actor.objects.all()
+    context = {'actors': data}
+    return render(request, 'moviesite/home.html', context)
